c107074f5a710363346cd236e885d0c9140ff082
MKonik <marta.maria.konik@gmail.com>
2018-11-13 23:10:56 +0100

    Change add box dialog UI

--- app/src/main/java/martakonik/flashcards/mainScreen/AddBoxDialog.kt
+++ app/src/main/java/martakonik/flashcards/mainScreen/AddBoxDialog.kt
@@ -1,25 +1,25 @@
 package martakonik.flashcards.mainScreen
 
+import android.app.Activity
 import android.app.AlertDialog
-import android.content.Context
-import android.widget.EditText
 import martakonik.flashcards.Database
 import martakonik.flashcards.R
 import martakonik.flashcards.data.Box
 
+
 class AddBoxDialog(
-        private val context: Context,
+        private val context: Activity,
         private val database: Database) {
 
     fun show() {
         val alert = AlertDialog.Builder(context)
-        val editText = EditText(context)
-        editText.hint = context.getString(R.string.dialog_hint)
-        alert.setTitle(context.getString(R.string.dialog_title))
+        val inflater = context.layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_add_box, null)
+        alert.setView(dialogView)
 
-        alert.setView(editText)
+        val editText = dialogView.findViewById(R.id.newBoxName) as android.support.design.widget.TextInputEditText
         alert.setCancelable(true)
-        alert.setPositiveButton(context.getString(R.string.save)) { _, _ ->
+        alert.setPositiveButton(context.getString(R.string.add)) { _, _ ->
             val boxName = editText.text.toString()
             val box = Box().apply {
                 name = boxName
