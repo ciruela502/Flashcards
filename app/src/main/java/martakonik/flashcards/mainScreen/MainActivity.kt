809fdea26c2bb7a06e9120c87d79543d0bca4d84
MKonik <marta.maria.konik@gmail.com>
2018-11-13 18:32:07 +0100

    Delete bottom nav bar

--- app/src/main/java/martakonik/flashcards/mainScreen/MainActivity.kt
+++ app/src/main/java/martakonik/flashcards/mainScreen/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
 
         val addBoxDialog = AddBoxDialog(this, database)
         binding.viewModel = MainViewModel(addBoxDialog, supportFragmentManager)
--- app/src/main/java/martakonik/flashcards/mainScreen/MainViewModel.kt
+++ app/src/main/java/martakonik/flashcards/mainScreen/MainViewModel.kt
@@ -1,39 +1,23 @@
 package martakonik.flashcards.mainScreen
 
-import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.view.View
 import martakonik.flashcards.R
 import martakonik.flashcards.boxList.BoxListFragment
-import martakonik.flashcards.learning.LearningFragment
 
 
 class MainViewModel(
         private val addBoxDialog: AddBoxDialog,
-        private val supportFragmentManager: FragmentManager
+        supportFragmentManager: FragmentManager
 ) {
     private val boxListFragment: Fragment = BoxListFragment()
-    private val learningFragment: Fragment = LearningFragment()
 
     init {
-        showFragment(boxListFragment)
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, boxListFragment).commit()
     }
 
     fun onAddClick(view: View) {
         addBoxDialog.show()
-    }
-
-    val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener{ item ->
-        when (item.itemId) {
-            R.id.flashcard_list -> showFragment(boxListFragment)
-            R.id.learning -> showFragment(learningFragment)
-        }
-        true
-    }
-
-    private fun showFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
